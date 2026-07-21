6a1c48b0b82aaccacfd0099292f2858956af2c19
Siddak Singh Bhatia <siddak1234@gmail.com>
2026-07-21 12:45:11 -0500

    Add: boats-to-save-people - submission-5

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        res=0
+        people.sort()
+
+
+        l, r = 0, len(people)-1
+        while l<=r:
+            diff = limit-people[r]
+            r-=1
+            res+=1
+            if people[l]<=diff:
+                l+=1
+        return res
+                    
